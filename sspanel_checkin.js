557e7636f6e4de83714877e8498417270f6e98ec
sudojia <i@imzjw.cn>
2024-05-17 20:03:44 +0800

    优化

--- sspanel_checkin.js
+++ sspanel_checkin.js
@@ -6,73 +6,60 @@
  * @last Modified time 2022/01/20 11:17
  * @description SSPANEL面板自动签到
  */
-const $ = new require('./env').Env('SSPANEL面板自动签到');
+const $ = require('./env').Env('SSPANEL面板自动签到');
 const notify = $.isNode() ? require('./sendNotify') : '';
-let total = process.env.SITE_ACCOUNTS, totalList = [], message = '';
-
-if (total.indexOf('&') > -1) {
-    totalList = total.split('&');
-} else {
-    totalList = [total];
-}
+let message = '';
+const axios = require('axios').default;
+const accountList = process.env.SITE_ACCOUNTS ? process.env.SITE_ACCOUNTS.split('&') : [];
 
 !(async () => {
-    if (!total) {
-        console.log('请先设置环境变量【SITE_ACCOUNTS】')
-        return;
+    if (!accountList) {
+        console.log('请先设置环境变量【SITE_ACCOUNTS】');
+        process.exit(1);
     }
-    for (let i = 0; i < totalList.length; i++) {
-        $.index = i + 1;
-        // 网站
-        $.SITE_URL = totalList[i].split(',')[0];
-        // 邮箱
-        $.email = totalList[i].split(',')[1].split(':')[0];
-        // 密码
-        $.pwd = totalList[i].split(',')[1].split(':')[1];
-        console.log(`\n*****开始第【${$.index}】个网站****\n`);
-        await main();
-        await $.wait(2000)
+    // 校验格式 网站1,邮箱1:密码1&网站2,邮箱2:密码2
+    const isValidFormat = accountList.every(account => {
+        const parts = account.split(',');
+        if (parts.length !== 2) {
+            return false;
+        }
+        const [site, credentials] = parts;
+        const credParts = credentials.split(':');
+        return !(credParts.length !== 2 || !site || !credentials);
+    });
+    if (!isValidFormat) {
+        console.error('格式错误，请确保遵循格式：网站1,邮箱1:密码1&网站2,邮箱2:密码2');
+        process.exit(1);
+    }
+    for (let i = 0; i < accountList.length; i++) {
+        const index = i + 1;
+        const [rawUrl, emailPwd] = accountList[i].split(',');
+        const [email, pwd] = emailPwd.split(':');
+        console.log(`\n*****开始第【${index}】个网站****\n`);
+        message += `第【${index}】个机场网站\n`;
+        const url = rawUrl.endsWith('/') ? rawUrl.slice(0, -1) : rawUrl;
+        await login(url, email, pwd);
+        if (1 === $.isRet) {
+            console.log('开始签到白嫖流量...\n')
+            await $.wait(1000);
+            await checkin(url);
+        }
+        await $.wait(2000);
     }
     if (message) {
         await notify.sendNotify(`${$.name}`, `${message}`);
     }
-})().catch((e) => {
-    $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
-}).finally(() => {
-    $.done();
-})
-
-async function main() {
-    await login();
-    await $.wait(1000);
-    if ($.isRet === 1) {
-        await checkin();
-    }
-}
+})();
 
 /**
  * 登录
  *
  * @returns {*}
  */
-function login() {
-    return new Promise((resolve) => {
-        $.post(sendPost('auth/login', `email=${$.email}&passwd=${$.pwd}&code=`), (err, response, data) => {
-            try {
-                if (err) {
-                    console.log(`login API 请求失败\n${JSON.stringify(err)}`)
-                } else {
-                    data = JSON.parse(data);
-                    console.log(data.msg, '\n');
-                    $.isRet = data.ret;
-                }
-            } catch (err) {
-                $.logErr(err, response);
-            } finally {
-                resolve();
-            }
-        })
-    })
+async function login(url, email, pwd) {
+    const data = await sendRequest(`${url}/auth/login`, 'post', `email=${email}&passwd=${pwd}&code=`);
+    // { ret: 1, msg: '登录成功' }
+    $.isRet = data.ret;
 }
 
 /**
@@ -80,48 +67,45 @@ function login() {
  *
  * @returns {*}
  */
-function checkin() {
-    return new Promise((resolve) => {
-        $.post(sendPost('user/checkin', ''), async (err, response, data) => {
-            try {
-                if (err) {
-                    console.log(`checkin API 请求失败\n${JSON.stringify(err)}`)
-                } else {
-                    console.log('开始进行签到...\n');
-                    data = JSON.parse(data);
-                    message += `开始第【${$.index}】个网站\n`;
-                    if (data.ret === 1) {
-                        if (data.trafficInfo) {
-                            console.log(`${data.msg}\n今日已用：${data.trafficInfo.todayUsedTraffic}\n过去已用：${data.trafficInfo.lastUsedTraffic}\n剩余流量：${data.trafficInfo.unUsedTraffic}\n\n`);
-                            message += `${data.msg}\n今日已用：${data.trafficInfo.todayUsedTraffic}\n过去已用：${data.trafficInfo.lastUsedTraffic}\n剩余流量：${data.trafficInfo.unUsedTraffic}\n\n`;
-                        } else {
-                            console.log(`${data.msg}\n`)
-                            message += `${data.msg}\n\n`;
-                        }
-                    } else {
-                        console.log(data.msg, '\n');
-                        message += data.msg + '\n\n';
-                    }
-                }
-            } catch (err) {
-                $.logErr(err, response);
-            } finally {
-                resolve();
-            }
-        })
-    })
+async function checkin(url) {
+    const data = await sendRequest(`${url}/user/checkin`, 'post', {});
+    if (1 === data.ret) {
+        console.log(`【签到信息】签到成功！获得${data.traffic}\n`)
+        console.log(`【今日已用】${data.trafficInfo.todayUsedTraffic}`)
+        console.log(`【过去已用】${data.trafficInfo.lastUsedTraffic}`)
+        console.log(`【剩余流量】${data.trafficInfo.unUsedTraffic}`)
+        message += `【签到信息】签到成功！获得${data.traffic}\n`
+        message += `【今日已用】${data.trafficInfo.todayUsedTraffic}\n`
+        message += `【过去已用】${data.trafficInfo.lastUsedTraffic}\n`
+        message += `【剩余流量】${data.trafficInfo.unUsedTraffic}\n\n`;
+    } else {
+        console.log('签到异常', data.msg);
+    }
 }
 
-function sendPost(path, body = {}) {
-    return {
-        url: `${$.SITE_URL}/${path}`,
-        body: body,
+/**
+ * 发送请求
+ *
+ * @param url 请求地址
+ * @param method 请求方法
+ * @param data 请求参数
+ * @returns {Promise<any>} 请求结果
+ */
+async function sendRequest(url, method, data = {}) {
+    const options = {
+        method,
+        url,
         headers: {
-            "Accept": "application/json, text/javascript, */*; q=0.01",
-            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "Referer": `${$.SITE_URL}/${path}`,
-            "Accept-encoding": "gzip, deflate, br",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62"
-        }
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
+        },
+        data
+    };
+    try {
+        const response = await axios(options);
+        return response.data;
+    } catch (error) {
+        console.error(`请求失败: ${error}`);
+        throw error;
     }
 }
--- steam_playtime.js
+++ steam_playtime.js
@@ -1,3 +1,4 @@
+const url = require("url");
 /**
  * @author Telegram@sudojia
  * @site https://blog.imzjw.cn
@@ -6,10 +7,22 @@
  * @last Modified time 2023/12/5 21:42
  * @description Steam 游玩时长查询
  */
-const $ = new require('./env').Env('Steam游玩时长查询');
+const $ = require('./env').Env('Steam两周内游玩明细');
 const notify = $.isNode() ? require('./sendNotify') : '';
+const axios = require('axios').default;
 let STEAM_TOKEN = process.env.STEAM_TOKEN, STEAM_64_ID = process.env.STEAM_64_ID, message = '';
 const STEAM_API = 'http://api.steampowered.com';
+
+const personaStateMap = {
+    1: '在线',
+    2: '忙碌',
+    3: '离开',
+    4: '暂停',
+    5: '想要交易',
+    6: '想要玩',
+    0: '离线', // 默认状态
+};
+
 !(async () => {
     if (!STEAM_TOKEN) {
         console.log('请先设置环境变量【STEAM_TOKEN】')
@@ -19,111 +32,75 @@ const STEAM_API = 'http://api.steampowered.com';
         console.log('请先设置环境变量【STEAM_64_ID】')
         return;
     }
-    await main();
-    await $.wait(1000);
-    if (message) {
-        await notify.sendNotify(`${$.name}`, `${message}`);
-    }
-})().catch((e) => {
-    $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
-}).finally(() => {
-    $.done();
-})
-
-async function main() {
     await getUser();
     await $.wait(800);
     await selectSteamTime();
-}
+    if (message) {
+        console.log(message);
+        await notify.sendNotify(`${$.name}`, `${message}`);
+    }
+})();
 
 /**
  * 获取 Steam 用户名 https://developer.valvesoftware.com/wiki/Steam_Web_API
+ *
  * @returns {Promise<unknown>}
  */
-function getUser() {
-    return new Promise((resolve) => {
-        $.get(sendGet('ISteamUser/GetPlayerSummaries/v0002/?key=' + STEAM_TOKEN + '&steamids=' + STEAM_64_ID), (error, response, data) => {
-            try {
-                if (error) {
-                    console.log(`API 请求失败\n请正确输入STEAM_TOKEN和STEAM_64_ID\n以为输出错误信息\n${JSON.stringify(error)}`)
-                } else {
-                    data = JSON.parse(data);
-                    // 获取用户名
-                    let personaName = data.response.players[0].personaname;
-                    // 获取当前状态
-                    let getPersonaState = data.response.players[0].personastate;
-                    // 默认为离线
-                    // 0: 离线 如果玩家的个人资料是私人的，则该值将始终为 "0"
-                    // 除非用户已将其状态设置为寻求交易或寻求玩游戏，因为即使个人资料是私人的，错误也会导致这些状态出现。
-                    let personaState = '离线';
-                    if (getPersonaState === 1) {
-                        personaState = '在线';
-                    } else if (getPersonaState === 2) {
-                        personaState = '忙碌';
-                    } else if (getPersonaState === 3) {
-                        personaState = '离开';
-                    } else if (getPersonaState === 4) {
-                        personaState = '暂停';
-                    } else if (getPersonaState === 5) {
-                        personaState = '想要交易';
-                    } else if (getPersonaState === 6) {
-                        personaState = '想要玩';
-                    }
-                    // 获取 Steam 个人主页 URL
-                    let profileUrl = data.response.players[0].profileurl;
-                    // 获取当前正在游玩的游戏
-                    let gameExtrainfo = data.response.players[0].gameextrainfo == null ? '当前没有在游玩' : '当前正在游玩【' + data.response.players[0].gameextrainfo + '】';
-                    message += '用户名【' + personaName + '】\n状态【' + personaState + '】\n主页：' + profileUrl + '\n' + gameExtrainfo + '\n'
-                }
-            } catch (error) {
-                $.logErr(error, response);
-            } finally {
-                resolve();
-            }
-        })
-    })
+async function getUser() {
+    const data = await sendRequest(`${STEAM_API}/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_TOKEN}&steamids=${STEAM_64_ID}`, 'get', {});
+    // 获取用户名
+    let personaName = data.response.players[0].personaname;
+    // 获取当前状态
+    let getPersonaState = data.response.players[0].personastate;
+    // 0: 离线 如果玩家的个人资料是私人的，则该值将始终为 "0" 、默认为 0
+    // 除非用户已将其状态设置为寻求交易或寻求玩游戏，因为即使个人资料是私人的，错误也会导致这些状态出现。
+    let personaState = personaStateMap[getPersonaState] || personaStateMap[0];
+    // 获取 Steam 个人主页 URL
+    let profileUrl = data.response.players[0].profileurl;
+    // 获取当前正在游玩的游戏
+    let gameExtrainfo = data.response.players[0].gameextrainfo == null ? '当前没有在游玩' : '当前正在游玩【' + data.response.players[0].gameextrainfo + '】';
+    message += `【用户名】${personaName}\n【状态】${personaState}\n【主页】${profileUrl}\n【当前游玩】${gameExtrainfo}\n\n`
 }
 
 /**
  * 获取游玩时长
  * @returns {Promise<unknown>}
  */
-function selectSteamTime() {
-    return new Promise((resolve) => {
-        $.get(sendGet('IPlayerService/GetRecentlyPlayedGames/v1?key=' + STEAM_TOKEN + '&steamid=' + STEAM_64_ID), (error, response, data) => {
-            try {
-                if (error) {
-                    console.log(`API 请求失败\n请正确输入STEAM_TOKEN和STEAM_64_ID\n以为输出错误信息\n${JSON.stringify(error)}`)
-                } else {
-                    data = JSON.parse(data);
-                    $.gamesList = data.response.games;
-                    message += '\n================游玩时长详情================'
-                    for (let g of $.gamesList) {
-                        // 获取总时长 分钟 / 60 = 小时
-                        let playtime_minute = g.playtime_forever / 60
-                        // 数据处理, 返回小时
-                        let playtime_hour = playtime_minute.toFixed(1)
-                        // 获取游戏名
-                        let game_name = g.name;
-                        message += '\n游戏名【' + game_name + '】\n总时长:' + playtime_hour + ' h' + '\n======';
-                    }
-                }
-            } catch (error) {
-                $.logErr(error, response);
-            } finally {
-                resolve();
-            }
-        })
-    })
+async function selectSteamTime() {
+    const data = await sendRequest(`${STEAM_API}/IPlayerService/GetRecentlyPlayedGames/v1?key=${STEAM_TOKEN}&steamid=${STEAM_64_ID}`, 'get', {});
+    let gameList = data.response.games;
+    message += `======过去两周内游玩情况======`
+    for (let g of gameList) {
+        // 获取总时长 分钟 / 60 = 小时
+        let playtime_minute = g.playtime_forever / 60
+        // 数据处理, 返回小时
+        let playtime_hour = playtime_minute.toFixed(1)
+        // 获取游戏名
+        let game_name = g.name;
+        message += '\n游戏名【' + game_name + '】\n总时长: ' + playtime_hour + ' h' + '\n================';
+    }
 }
 
 /**
- * 封装 HTTP GET 请求
- * @param path
- * @returns {{headers: {}, url: string}}
+ * 发送请求
+ *
+ * @param url 请求地址
+ * @param method 请求方法
+ * @param data 请求参数
+ * @returns {Promise<any>} 请求结果
  */
-function sendGet(path) {
-    return {
-        url: `${STEAM_API}/${path}`, headers: {}
+async function sendRequest(url, method, data = {}) {
+    const options = {
+        method,
+        url,
+        headers: {},
+        data
+    };
+    try {
+        const response = await axios(options);
+        return response.data;
+    } catch (error) {
+        console.error(`请求失败: ${error}`);
+        throw error;
     }
 }
